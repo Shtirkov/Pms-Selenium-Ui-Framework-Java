3ebe298aba18f54dcb2dc667f9ce4889976f8ecc
Shtirkov <shtirkov96@gmail.com>
2024-01-27 22:44:58 +0200

    Synchronization issues fixed and added verification on which products are added to the cart

--- PmsSeleniumUiFramework/src/test/java/pms/PmsSeleniumUiFramework/Tests.java
+++ PmsSeleniumUiFramework/src/test/java/pms/PmsSeleniumUiFramework/Tests.java
@@ -1,9 +1,14 @@
 package pms.PmsSeleniumUiFramework;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -24,13 +29,23 @@ public class Tests {
 
 		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ngx-pagination")));
 		List<WebElement> productsList = driver.findElements(By.xpath("//div[@class='card']"));
-
-		for(WebElement product : productsList) {
-			w.until(ExpectedConditions.invisibilityOf(driver.findElement(By.id("toast-container"))));
-			product.findElement(By.cssSelector(".w-10")).click();
-			w.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("toast-container"))));
-		}
+		List<String> addedProducts = new ArrayList<String>();
 		
+		productsList.stream().forEach(p -> {
+			w.until(ExpectedConditions.invisibilityOf(driver.findElement(By.id("toast-container"))));
+			addedProducts.add(p.findElement(By.cssSelector("h5")).getText());
+			p.findElement(By.cssSelector(".w-10")).click();
+			w.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("toast-container"))));
+		});
+		
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		w.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//button[text()='Checkout']"))));
+
+		List<WebElement> itemsInTheCart = driver.findElements(By.cssSelector(".cartWrap h3"));
+		List<String> itemsInTheCartAsString = itemsInTheCart.stream().map(i -> i.getText()).collect(Collectors.toList());
+				
+		Assert.assertEquals(itemsInTheCartAsString, addedProducts);
+				
 		driver.quit();
 	}
 
